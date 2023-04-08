16fe0e4be4ae1f1e015d0b94833bcb616b163ae6
rgsk <guptarahul70322@gmail.com>
2023-04-08 14:27:38 +0530

    absolute imports working

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 require('dotenv').config();
 // api/index.ts
-import { environmentVariables } from './lib/environmentVariables';
+import { environmentVariables } from 'lib/environmentVariables';
 import { server } from './server';
 server.listen({ port: environmentVariables.PORT }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
