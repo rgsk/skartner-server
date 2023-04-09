7bac961b6b49e74a0e94b1682b8e76b23de8d401
rgsk <guptarahul70322@gmail.com>
2023-04-10 01:08:11 +0530

    settings.json vscode

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import { config } from 'dotenv';
 config();
 // api/index.ts
-import { server } from './server';
 import environmentVars from 'lib/environmentVars';
+import { server } from './server';
 server.listen({ port: environmentVars.PORT }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
