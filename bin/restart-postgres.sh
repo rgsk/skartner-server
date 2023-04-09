49f36ab97f1c598273e8fd23c5468852bcffe43b
rgsk <guptarahul70322@gmail.com>
2023-04-09 13:44:56 +0530

    local postgres

--- bin/restart-postgres.sh
+++ bin/restart-postgres.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 
-docker rm -f skartner-server-pg || true
+docker rm -f skartner-server-local || true
 
 docker run \
 	--rm \
-	-p 5432:5432 \
-	--name skartner-server-pg \
+	-p 5433:5432 \
+	--name skartner-server-local \
 	-e POSTGRES_PASSWORD=postgres \
 	-e POSTGRES_USER=postgres \
 	-e POSTGRES_DB=postgres \
