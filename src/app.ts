c3dcf5d6e4b86d8d3e3f158b21a4ef54a4ddd739
rgsk <guptarahul70322@gmail.com>
2023-04-30 21:31:47 +0530

    minor

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ config();
 // api/index.ts
 import environmentVars from 'lib/environmentVars';
 import { server } from './server';
+
 server.listen({ port: environmentVars.PORT }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
+// single line and comment added
