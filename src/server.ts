530848bb0d2b14e734408b0cae02cceeb9a77e4d
rgsk <guptarahul70322@gmail.com>
2023-05-15 09:46:45 +0530

    cors setup

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,10 @@ export const server = new ApolloServer({
   schema,
   context,
   cache: 'bounded',
+  cors: {
+    origin: 'https://skartner-web.vercel.app', // Replace with the allowed origin(s)
+    credentials: true, // Set to true if you're using cookies or authentication headers
+  },
   // @ts-ignore
   // plugins: [BASIC_LOGGING],
 });
