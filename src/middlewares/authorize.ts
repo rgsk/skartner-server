343a25c64445ff7ca637d99e0a020bec6a4a5798
rgsk <guptarahul70322@gmail.com>
2023-06-29 11:13:13 +0530

    checkUserAuthorizedForPermission function

--- src/middlewares/authorize.ts
+++ src/middlewares/authorize.ts
@@ -1,6 +1,110 @@
 import { db } from 'db';
 import { NextFunction, Request, Response } from 'express';
 
+export const checkUserAuthorizedForPermission = async ({
+  permissionName,
+  userId,
+}: {
+  permissionName: string;
+  userId: string;
+}) => {
+  const noEntryIsFalse = await db.permission.count({
+    where: {
+      name: permissionName,
+      relationPermissionToUserAsPermission: {
+        none: { userId: userId, isAllowed: false },
+      },
+      relationPermissionToRoleAsPermission: {
+        none: {
+          role: {
+            relationRoleToUserAsRole: { some: { userId: userId } },
+          },
+          isAllowed: false,
+        },
+      },
+      permissionHierarchyAsChild: {
+        every: {
+          parentPermission: {
+            relationPermissionToUserAsPermission: {
+              none: { userId: userId, isAllowed: false },
+            },
+            relationPermissionToRoleAsPermission: {
+              none: {
+                role: {
+                  relationRoleToUserAsRole: {
+                    some: { userId: userId },
+                  },
+                },
+                isAllowed: false,
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+  //   console.log({ noEntryIsFalse });
+  if (!noEntryIsFalse) {
+    return false;
+  }
+  const someEntryIsTrue = await db.permission.count({
+    where: {
+      name: permissionName,
+      OR: [
+        {
+          relationPermissionToUserAsPermission: {
+            some: { userId: userId, isAllowed: true },
+          },
+        },
+        {
+          relationPermissionToRoleAsPermission: {
+            some: {
+              role: {
+                relationRoleToUserAsRole: {
+                  some: { userId: userId },
+                },
+              },
+              isAllowed: true,
+            },
+          },
+        },
+        {
+          permissionHierarchyAsChild: {
+            some: {
+              parentPermission: {
+                OR: [
+                  {
+                    relationPermissionToUserAsPermission: {
+                      some: { userId: userId, isAllowed: true },
+                    },
+                  },
+                  {
+                    relationPermissionToRoleAsPermission: {
+                      some: {
+                        role: {
+                          relationRoleToUserAsRole: {
+                            some: { userId: userId },
+                          },
+                        },
+                        isAllowed: true,
+                      },
+                    },
+                  },
+                ],
+              },
+            },
+          },
+        },
+      ],
+    },
+  });
+  //   console.log({ someEntryIsTrue });
+  if (someEntryIsTrue) {
+    return true;
+  }
+  return false;
+};
+
 const authorize =
   (permissionName: string) =>
   async (req: Request, res: Response, next: NextFunction) => {
@@ -8,98 +112,11 @@ const authorize =
       // get the user via token
       const user = await db.user.findUnique({ where: { email: 'rahul' } });
       if (user) {
-        const noEntryIsFalse = await db.permission.count({
-          where: {
-            name: permissionName,
-            relationPermissionToUserAsPermission: {
-              none: { userId: user.id, isAllowed: false },
-            },
-            relationPermissionToRoleAsPermission: {
-              none: {
-                role: {
-                  relationRoleToUserAsRole: { some: { userId: user.id } },
-                },
-                isAllowed: false,
-              },
-            },
-            permissionHierarchyAsChild: {
-              every: {
-                parentPermission: {
-                  relationPermissionToUserAsPermission: {
-                    none: { userId: user.id, isAllowed: false },
-                  },
-                  relationPermissionToRoleAsPermission: {
-                    none: {
-                      role: {
-                        relationRoleToUserAsRole: {
-                          some: { userId: user.id },
-                        },
-                      },
-                      isAllowed: false,
-                    },
-                  },
-                },
-              },
-            },
-          },
+        const isAuthorized = await checkUserAuthorizedForPermission({
+          userId: user.id,
+          permissionName,
         });
-        console.log({ noEntryIsFalse });
-        if (!noEntryIsFalse) {
-          throw new Error('Un-Authorized');
-        }
-        const someEntryIsTrue = await db.permission.count({
-          where: {
-            name: permissionName,
-            OR: [
-              {
-                relationPermissionToUserAsPermission: {
-                  some: { userId: user.id, isAllowed: true },
-                },
-              },
-              {
-                relationPermissionToRoleAsPermission: {
-                  some: {
-                    role: {
-                      relationRoleToUserAsRole: {
-                        some: { userId: user.id },
-                      },
-                    },
-                    isAllowed: true,
-                  },
-                },
-              },
-              {
-                permissionHierarchyAsChild: {
-                  some: {
-                    parentPermission: {
-                      OR: [
-                        {
-                          relationPermissionToUserAsPermission: {
-                            some: { userId: user.id, isAllowed: true },
-                          },
-                        },
-                        {
-                          relationPermissionToRoleAsPermission: {
-                            some: {
-                              role: {
-                                relationRoleToUserAsRole: {
-                                  some: { userId: user.id },
-                                },
-                              },
-                              isAllowed: true,
-                            },
-                          },
-                        },
-                      ],
-                    },
-                  },
-                },
-              },
-            ],
-          },
-        });
-        console.log({ someEntryIsTrue });
-        if (someEntryIsTrue) {
+        if (isAuthorized) {
           return next();
         }
       }
