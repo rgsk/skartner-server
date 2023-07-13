6c962d19f178e60ae67212c0e3c45e470e7f5b84
rgsk <guptarahul70322@gmail.com>
2023-07-13 22:16:25 +0530

    minor

--- src/lib/environmentVars.ts
+++ src/lib/environmentVars.ts
@@ -22,9 +22,9 @@ const environmentVarsSchema = z.object({
 const fields = {
   PORT: process.env.PORT,
   DATABASE_URL: process.env.DATABASE_URL,
-  REDIS_URL: process.env.REDIS_URL,
   NODE_ENV: process.env.NODE_ENV,
   LOCAL_IP: process.env.LOCAL_IP,
+  REDIS_URL: process.env.REDIS_URL,
   OPENAI_API_KEY: process.env.OPENAI_API_KEY,
   OPENAI_ORGANIZATION: process.env.OPENAI_ORGANIZATION,
 };
