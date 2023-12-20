9e590fb76e3426e4a5df4215d2087385eb25afde
rgsk <guptarahul70322@gmail.com>
2023-12-20 14:57:36 +0530

    deploy command modified

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,1 +1,9 @@
-docker build . -t rgskartner/skartner-server
+docker build . -t rgskartner/skartner-server --platform linux/amd64
+
+docker push rgskartner/skartner-server
+
+source ../skartner-instance/envs/skartner-jenkins/staging.env
+
+# echo "JENKINS_API_KEY: $JENKINS_API_KEY"
+
+curl -X POST http://jenkins.skartner.com/job/restart-skartner-server/build --user dev-rahul:$JENKINS_API_KEY
