6399eff502cc1644dbbbc42b4fa3358a091e0198
rgsk <guptarahul70322@gmail.com>
2024-05-08 17:12:14 +0530

    transcription and translation api

--- src/routers/generalRouter.ts
+++ src/routers/generalRouter.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
+import fs from 'fs';
 import openAI from 'lib/openAI';
 import { getPresignedUrl, uploadAudio } from 'lib/thirdPartyUtils';
+import multer from 'multer';
+import path from 'path';
 import { v4 } from 'uuid';
 import { z } from 'zod';
 const generalRouter = Router();
@@ -25,6 +28,61 @@ generalRouter.post('/text-to-speech', async (req, res, next) => {
     next(err);
   }
 });
+const dest = path.join(__dirname, 'uploads');
+const storage = multer.diskStorage({
+  destination: dest,
+  filename: function (req, file, cb) {
+    let extArray = file.mimetype.split('/');
+    let extension = extArray[extArray.length - 1];
+    cb(
+      null,
+      file.fieldname +
+        '-' +
+        file.originalname +
+        '-' +
+        Date.now() +
+        '.' +
+        extension
+    );
+  },
+});
+const upload = multer({
+  storage: storage,
+});
+
+generalRouter.post(
+  '/speech-to-text',
+  upload.single('audio'),
+  async (req, res, next) => {
+    try {
+      if (req.file) {
+        let data = fs.createReadStream(req.file.path);
+        const transcription = await openAI.audio.transcriptions.create({
+          model: 'whisper-1',
+          file: data,
+        });
+        data = fs.createReadStream(req.file.path);
+        const translation = await openAI.audio.translations.create({
+          model: 'whisper-1',
+          file: data,
+        });
+        await new Promise((resolve, reject) => {
+          fs.unlink(req.file!.path, (err) => {
+            if (err) return reject(err);
+            resolve(null);
+          });
+        });
+        return res.json({
+          transcription: transcription.text,
+          translation: translation.text,
+        });
+      }
+      throw new Error('no file provided');
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
 generalRouter.get('/presigned-url', async (req, res, next) => {
   try {
